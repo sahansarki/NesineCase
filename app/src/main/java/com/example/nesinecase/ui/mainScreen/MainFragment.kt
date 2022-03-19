377a8d913ddb5f29205e92158a8dad363ed7dd58
Sahan Sarki <sahancansarki@gmail.com>
2022-03-19 13:35:49 +0300

    First Screen was created.

---
+++ app/src/main/java/com/example/nesinecase/ui/mainScreen/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.nesinecase.ui.mainScreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nesinecase.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
