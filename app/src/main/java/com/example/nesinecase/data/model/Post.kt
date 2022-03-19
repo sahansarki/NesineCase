76213d719089bb7abdec54551421618ad867b7b4
Sahan Sarki <sahancansarki@gmail.com>
2022-03-19 19:06:06 +0300

    Response type was changed.

--- app/src/main/java/com/example/nesinecase/data/model/Post.kt
+++ app/src/main/java/com/example/nesinecase/data/model/Post.kt
@@ -1,12 +1,17 @@
 package com.example.nesinecase.data.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Post(
+    @SerializedName("userId")
     val userId: Int,
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("body")
     val body: String
 ): Parcelable
--- app/src/main/java/com/example/nesinecase/data/response/PostResponse.kt
+++ app/src/main/java/com/example/nesinecase/data/response/PostResponse.kt
@@ -2,6 +2,10 @@ package com.example.nesinecase.data.response
 
 import com.example.nesinecase.data.model.Post
 
+/*
 data class PostResponse(
     val postList: List<Post>
 )
+
+ */
+class PostResponse : ArrayList<Post>()
