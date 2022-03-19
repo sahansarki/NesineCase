7f77aa96f34d9537ebe2256b8be0681d02843a78
Sahan Sarki <sahancansarki@gmail.com>
2022-03-19 19:04:51 +0300

    Internet URL's were created.

---
+++ app/src/main/java/com/example/nesinecase/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.nesinecase.util
+
+object Constants{
+    const val POST_URL = "https://jsonplaceholder.typicode.com/"
+    const val PHOTO_URL = "https://picsum.photos/300/300?random="
+}
