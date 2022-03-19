1b114d46d63dcce0e13e712f0ec2f8d028c4d01a
Sahan Sarki <sahancansarki@gmail.com>
2022-03-19 21:01:26 +0300

    Detail Bottom Sheet class was filled.

--- app/src/main/java/com/example/nesinecase/ui/fragment/detailScreen/DetailBottomSheetFragment.kt
+++ app/src/main/java/com/example/nesinecase/ui/fragment/detailScreen/DetailBottomSheetFragment.kt
@@ -1,8 +1,58 @@
 package com.example.nesinecase.ui.fragment.detailScreen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.navArgs
+import com.example.nesinecase.R
+import com.example.nesinecase.data.model.Post
+import com.example.nesinecase.databinding.FragmentDetailBottomSheetBinding
+import com.example.nesinecase.extension.loadImage
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailBottomSheetFragment: BottomSheetDialogFragment() {
+class DetailBottomSheetFragment(
+    private val updateCallback: (post: Post) -> Unit
+) : BottomSheetDialogFragment() {
+
+    private lateinit var bottomSheetBinding: FragmentDetailBottomSheetBinding
+    private lateinit var post: Post
+
+    companion object {
+        const val TAG = "DetailBottomSheetFragment"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.ThemeOverlay_BottomSheetDialog)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        bottomSheetBinding = FragmentDetailBottomSheetBinding.inflate(layoutInflater)
+        return bottomSheetBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val safeArgs: DetailBottomSheetFragmentArgs by navArgs()
+        post = safeArgs.post
+        bottomSheetBinding.post = post
+        bottomSheetBinding.postImage.loadImage(post.url)
+
+        bottomSheetBinding.updateButton.setOnClickListener {
+            post.title = bottomSheetBinding.postTitle.text.toString()
+            post.body = bottomSheetBinding.postBody.text.toString()
+            updateCallback(post)
+        }
+
+    }
+
 }
