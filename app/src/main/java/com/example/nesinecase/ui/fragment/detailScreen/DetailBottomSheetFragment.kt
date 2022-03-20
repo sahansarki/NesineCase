25eb39bc13fba53d4edc4ebe9f0e2fbc4b7ce2fe
Sahan Sarki <sahancansarki@gmail.com>
2022-03-20 17:57:01 +0300

    unused import was deleted.

--- app/src/main/java/com/example/nesinecase/ui/fragment/detailScreen/DetailBottomSheetFragment.kt
+++ app/src/main/java/com/example/nesinecase/ui/fragment/detailScreen/DetailBottomSheetFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.fragment.navArgs
 import com.example.nesinecase.R
 import com.example.nesinecase.data.model.Post
 import com.example.nesinecase.databinding.FragmentDetailBottomSheetBinding
