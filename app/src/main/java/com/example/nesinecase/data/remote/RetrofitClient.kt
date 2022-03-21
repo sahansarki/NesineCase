a6f45b0fa09429dc3686cefe4d9409483e80905a
Sahan Sarki <sahancansarki@gmail.com>
2022-03-21 16:00:31 +0300

    to use RxJava RxJava2CallAdapterFactory was added to retrofit instance.

--- app/src/main/java/com/example/nesinecase/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/nesinecase/data/remote/RetrofitClient.kt
@@ -2,6 +2,7 @@ package com.example.nesinecase.data.remote
 
 import com.example.nesinecase.util.Constants
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 abstract class RetrofitClient {
@@ -13,6 +14,7 @@ abstract class RetrofitClient {
                 val instance = Retrofit.Builder()
                     .baseUrl(Constants.POST_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build()
 
                 INSTANCE = instance
