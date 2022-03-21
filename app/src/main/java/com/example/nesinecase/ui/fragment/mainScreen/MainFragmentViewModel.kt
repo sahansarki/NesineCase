93d27114497fcece360abb7ee4ecd3654ea28c85
Sahan Sarki <sahancansarki@gmail.com>
2022-03-21 15:58:21 +0300

    coroutine scope was deleted.

--- app/src/main/java/com/example/nesinecase/ui/fragment/mainScreen/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/nesinecase/ui/fragment/mainScreen/MainFragmentViewModel.kt
@@ -2,15 +2,12 @@ package com.example.nesinecase.ui.fragment.mainScreen
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.example.nesinecase.data.response.PostResponse
 import com.example.nesinecase.di.PostRepositoryImpl
 import com.example.nesinecase.repository.PostRepository
 import com.example.nesinecase.ui.adapter.base.BaseViewModel
 import com.example.nesinecase.util.DataHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,8 +24,9 @@ class MainFragmentViewModel @Inject constructor(
     }
 
     fun getAllPosts() {
-        viewModelScope.launch(Dispatchers.Main) {
-            mutablePostList.value = postRepository.getAllPosts()
+        postRepository.getAllPosts {
+            mutablePostList.value = it
         }
+
     }
 }
